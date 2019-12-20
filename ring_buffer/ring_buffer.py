5cfa8d191ea82137eb9096f5887dc60ea543d0ec
Neha Patel <neha.r.p@gmail.com>
2019-12-20 09:01:34 -0800

    working on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,7 +8,16 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #set current to item that is going to be added
+        self.current = item
+        #if ring buffer is full, overwrite the oldest node(tail) and add to front
+        if self.storage.length == self.capacity:
+            self.storage.tail.value = self.current
+            self.storage.move_to_front(self.storage.tail)
+            return
+        #if not add to head
+        self.storage.add_to_head(self.current)
+
 
     def get(self):
         # Note:  This is the only [] allowed
